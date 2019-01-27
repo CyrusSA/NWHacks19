173381936b3b24e30202ba7fcc31d6c133f6cfc9
Cyrus Arora <cyrus.s.arora@gmail.com>
2019-01-26 21:50:52 -0800

    Room name

--- nwhacks19/api/common/storage.js
+++ nwhacks19/api/common/storage.js
@@ -1,40 +1,49 @@
-const config = require('../common/config');
+const config = require("../common/config");
 
-const kv = require('lib')({token: process.env.STDLIB_LIBRARY_TOKEN}).utils({table: config.kv_table}).kv;
+const kv = require("lib")({ token: process.env.STDLIB_LIBRARY_TOKEN }).utils({
+  table: config.kv_table
+}).kv;
 
-const empty_room = {
+const empty_room = (name, qr) => ({
   now_playing: null,
   voting_pool: [],
-}
+  name: name,
+  qr: qr
+});
 
 const empty_suggestion = (name, id) => ({
   upvotes: 0,
   downvotes: 0,
   name: name,
-  id: id,
-})
+  id: id
+});
 
 const storage = {
-  get_room: (room_id) => kv.get({key: room_id }),
-  add_room: (room_id) => kv.set({ key: room_id, value: empty_room }),
-  clear_room: (room_id) => kv.clear({key: room_id}),
+  get_room: room_id => kv.get({ key: room_id }),
+  add_room: (room_id, name, qr) =>
+    kv.set({
+      key: room_id,
+      value: { now_playing: null, voting_pool: [], name: name, qr: qr }
+    }),
+  clear_room: room_id => kv.clear({ key: room_id }),
   get_all: () => kv.entries(),
   // generic update, for internal use only (mainly) because it's unsafe
   update_room: (room_id, value) => kv.set({ key: room_id, value }),
-  add_suggestion: (room_id, name, id) => storage.map(room_id, c => {
-    c.voting_pool.push({
-      upvotes: 1,
-      downvotes: 0,
-      name,
-      id,
-    })
-    return c
-  }),
+  add_suggestion: (room_id, name, id) =>
+    storage.map(room_id, c => {
+      c.voting_pool.push({
+        upvotes: 1,
+        downvotes: 0,
+        name,
+        id
+      });
+      return c;
+    }),
   // modify the value at the key using f. Returns the previous value, although if f modifies it those will remain
   map: async (key, f) => {
-    const old_val = await kv.get({ key })
-    await kv.set({ key, value: f(old_val) })
-    return old_val
-  },
-}
-module.exports = storage
+    const old_val = await kv.get({ key });
+    await kv.set({ key, value: f(old_val) });
+    return old_val;
+  }
+};
+module.exports = storage;
--- nwhacks19/api/functions/create_room.js
+++ nwhacks19/api/functions/create_room.js
@@ -1,18 +1,19 @@
-const qrcode = require('qrcode');
-const idable = require('idable');
+const qrcode = require("qrcode");
+const idable = require("idable");
 
-const config = require('../common/config');
-const storage = require('../common/storage');
+const config = require("../common/config");
+const storage = require("../common/storage");
 
 /**
-* Creates a new room with random ID, returning the ID and a qr code to match
-*/
-module.exports = async () => {
-  const room_id = idable(config.room_id_length)
-  storage.add_room(room_id)
-  const qr_data_url = await qrcode.toDataURL(room_id)
+ * Creates a new room with specified name, random ID, returning the ID and a qr code to match
+ * @param {string} name
+ */
+module.exports = async name => {
+  const room_id = idable(config.room_id_length);
+  const qr_data_url = await qrcode.toDataURL(room_id);
+  await storage.add_room(room_id, name, qr_data_url);
   return {
     room_id,
-    qr_data_url,
-  }
-}
+    qr_data_url
+  };
+};
