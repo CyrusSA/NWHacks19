b23a7df113d714763b1fc8b73e6fd0499d8eb498
Mark Polyakov <mark@markasoftware.com>
2019-01-27 10:31:00 -0800

    switch to vanilla youtube-search

--- nwhacks19/api/functions/search.js
+++ nwhacks19/api/functions/search.js
@@ -1,6 +1,6 @@
 const config = require("../common/config");
 
-const youtube_search = require("youtube-search-promise");
+const youtube_search = require("youtube-search");
 
 const opts = {
   maxResults: config.max_search_results,
@@ -14,10 +14,16 @@ console.log(process.env.YT_API_KEY);
  * @param {string} q
  */
 module.exports = async q => {
-  const yt_res = await youtube_search(q, opts);
-  return yt_res.map(c => ({
-    name: c.title,
-    id: c.id,
-    thumbnail: c.thumbnails.medium.url
-  }));
-};
+  const yt_res = await new Promise((resolve, reject) => {
+    youtube_search(q, opts, (err, res) => {
+      if (err) return reject(err)
+      resolve(res.map(c => ({
+          name: c.title,
+          id: c.id,
+          thumbnail: c.thumbnails.medium.url
+        }))
+      );
+    })
+  });
+  return yt_res
+}
